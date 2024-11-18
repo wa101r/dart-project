3b197e85e9f1135ae774f92a26c5bbd95a259c83
wa101r <wallerdaiki@gmail.com>
2024-11-18 15:10:20 +0700

    commit

---
+++ reminder.dart.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  int num1 = 34;
+  int num2 = 5;
+
+  int div1 = num1 ~/ num2;
+  int div2 = num1 % num2 ;
+
+  print("outpt");
+  print("quotient = $div1");
+  print("remainder= $div2");
+}
