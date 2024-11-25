e8530ac7aa9ec66243b494fb0b158c7e907fc1a2
wa101r <wallerdaiki@gmail.com>
2024-11-25 15:44:49 +0700

    Update question2-4.dart

--- week2/question2-4.dart
+++ week2/question2-4.dart
@@ -1,7 +1,7 @@
 void main() {
 
   for (int multiplier = 1; multiplier <= 9; multiplier++) {
-    print("สูตรคูณแม่ $multiplier:");
+    print("Maternal multiplication formula $multiplier:");
     for (int i = 1; i <= 12; i++) {
       int result = multiplier * i;
       print("$multiplier x $i = $result");
